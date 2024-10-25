705f898cfe58f322e4a7279e87ae5b252ce66995
Евгений Страхов <goodwin.s@mail.ru>
2024-10-25 18:12:41 +0300

    Initial commit

---
+++ src/test/java/pro/sky/courseworkUsingSpring/CourseworkUsingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.courseworkUsingSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkUsingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
