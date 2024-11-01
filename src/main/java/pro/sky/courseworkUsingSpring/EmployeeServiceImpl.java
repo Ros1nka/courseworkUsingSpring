c0d1f1a883c4dd14e342e78d412e5628906d7d8b
Eugeny <93324945+Ros1nka@users.noreply.github.com>
2024-11-01 20:23:32 +0300

    Update EmployeeServiceImpl.java
    update 01.11

--- src/main/java/pro/sky/courseworkUsingSpring/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/courseworkUsingSpring/EmployeeServiceImpl.java
@@ -13,32 +13,47 @@ public class EmployeeServiceImpl implements EmployeeService {
     List<Employee> employees = new ArrayList<>();
     final int maxEmployees = 10;
 
+    @Override
     public Employee addEmployee(String firstName, String lastName) {
         if (employees.size() >= maxEmployees) {
             throw new EmployeeStorageIsFullException("Превышено максимальное количество сотрудников");
         }
         Employee employee = new Employee(firstName, lastName);
 
-        if (findEmployee(firstName, lastName)) {
+        if (employees.contains(employee)) {
             throw new EmployeeAlreadyAddedException("Сотрудник уже добавлен");
         }
         employees.add(employee);
         return employee;
     }
 
-    public void removeEmployee(String firstName, String lastName) {
+    @Override
+    public Employee removeEmployee(String firstName, String lastName) {
+		Employee employee = new Employee(firstName, lastName);
         try {
-            findEmployee(firstName, lastName);
-        } catch (EmployeeNotFoundException e) {
+            employees.remove(employee);
+        } catch (Exception e) {
             throw new EmployeeNotFoundException("Сотрудник не найден");
         }
+		return employee;
+		
+		//if(!employees.contains(employee)){
+		//	throw new EmployeeNotFoundException;
+		//}
+		//return employee;
     }
 
-    public boolean findEmployee(String firstName, String lastName) {
+    @Override
+    public Employee findEmployee(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
         if (employees.contains(employee)) {
-            return true;
+            return employee;
         }
         throw new EmployeeNotFoundException("Сотрудник не найден");
     }
+	
+	@Override
+	public List<Employee> getAllEmployee(){
+        return employees;
+	}
 }
