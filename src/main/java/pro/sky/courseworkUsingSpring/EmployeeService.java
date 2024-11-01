c7bbe51e133fb37c52fbea751086a4141efef229
Eugeny <93324945+Ros1nka@users.noreply.github.com>
2024-11-01 20:24:17 +0300

    Update EmployeeService.java
    update 01/11

--- src/main/java/pro/sky/courseworkUsingSpring/EmployeeService.java
+++ src/main/java/pro/sky/courseworkUsingSpring/EmployeeService.java
@@ -2,7 +2,9 @@ package pro.sky.courseworkUsingSpring;
 public interface EmployeeService {
     Employee addEmployee(String firstName, String lastName);
 
-    void removeEmployee(String firstName, String lastName);
+    Employee removeEmployee(String firstName, String lastName);
 
-    boolean findEmployee(String firstName, String lastName);
+    Employee findEmployee(String firstName, String lastName);
+
+    List<Employee> getAllEmployee()
 }
