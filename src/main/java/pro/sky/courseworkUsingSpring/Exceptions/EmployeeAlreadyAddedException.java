9d4d6f9ebd0c32c16b213dab7007d7818c0bb653
Eugeny <93324945+Ros1nka@users.noreply.github.com>
2024-11-02 04:49:44 +0300

    Update EmployeeAlreadyAddedException.java

--- src/main/java/pro/sky/courseworkUsingSpring/Exceptions/EmployeeAlreadyAddedException.java
+++ src/main/java/pro/sky/courseworkUsingSpring/Exceptions/EmployeeAlreadyAddedException.java
@@ -3,7 +3,7 @@ package pro.sky.courseworkUsingSpring.Exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)
+@ResponseStatus(HttpStatus.CONFLICT, reason = "Сотрудник уже добавлен")
 public class EmployeeAlreadyAddedException extends RuntimeException {
     public EmployeeAlreadyAddedException(String message) {
         super(message);
