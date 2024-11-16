c3364e69205856778d073242bc13aa2ef3d06909
Eugeny <goodwin.s@mail.ru>
2024-11-16 19:40:44 +0300

    update 16/11

--- src/main/java/pro/sky/courseworkUsingSpring/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/courseworkUsingSpring/EmployeeServiceImpl.java
@@ -14,16 +14,20 @@ import java.util.Map;
 public class EmployeeServiceImpl implements EmployeeService {
     final int maxEmployees = 5;
 
-    Map<String, Employee> employees = new HashMap(Map.of(
+    Map<String, Employee> employees = new HashMap<>(Map.of(
+            "Владимир" + "Машков",
             new Employee("Владимир", "Машков"),
+            "Константин" + "Хабенский",
             new Employee("Константин", "Хабенский"),
+            "Елена" + "Яковлева",
             new Employee("Елена", "Яковлева"),
+            "Игорь" + "Петренко",
             new Employee("Игорь", "Петренко")
     ));
-//add?firstname=wqeda&lastname=qwewqeqwe
+
     @Override
     public Employee addEmployee(String firstName, String lastName) {
-		
+
         if (employees.size() >= maxEmployees) {
             throw new EmployeeStorageIsFullException();
         }
@@ -38,27 +42,27 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee removeEmployee(String firstName, String lastName) {
-		Employee employee = new Employee(firstName, lastName);
-		
-		if(!employees.contains(employee)){
-		    throw new EmployeeNotFoundException();
-		}
-		employees.remove(employee);
-		return employee;
+        Employee employee = new Employee(firstName, lastName);
+
+        if (!employees.contains(employee)) {
+            throw new EmployeeNotFoundException();
+        }
+        employees.remove(employee);
+        return employee;
     }
 
     @Override
     public Employee findEmployee(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-		
+
         if (employees.contains(employee)) {
             return employee;
         }
         throw new EmployeeNotFoundException();
     }
-	
-	@Override
-	public List<Employee> getAllEmployee(){
+
+    @Override
+    public List<Employee> getAllEmployee() {
         return employees;
-	}
+    }
 }
